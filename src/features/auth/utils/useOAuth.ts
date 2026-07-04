83a58517d6bdfea6d4919ad938c059aa0a9c77a5
samuelkime267 <samuelkime7@gmail.com>
2026-07-04 23:52:09 +0100

    final clean up

--- src/features/auth/utils/useOAuth.ts
+++ src/features/auth/utils/useOAuth.ts
@@ -1,6 +1,5 @@
 import { useEffect } from "react";
-import { BACKEND_URL, BASE_URL } from "@/config/env";
-// import { DEFAULT_REDIRECT_ROUTE } from "@/data/routes.data";
+import { BACKEND_URL } from "@/config/env";
 import { AUTH_REFRESH_TOKEN, GOOGLE_AUTH } from "@/data/routes";
 import { useAuth } from "@/stores";
 import api from "@/lib/api";
@@ -11,8 +10,6 @@ export default function useOAuth(
   const { setToken } = useAuth((s) => s);
   const loginWithGoogle = async () => {
     setError(undefined);
-    console.log(`${BASE_URL}`);
-    console.log(`${GOOGLE_AUTH}`);
     window.open(GOOGLE_AUTH, "_blank", "width=500,height=600");
   };
 
@@ -22,9 +19,7 @@ export default function useOAuth(
       if (event.data.from === "oauth") {
         if (event.data.success) {
           const { data } = await api.post(AUTH_REFRESH_TOKEN);
-          console.log("OAuth success:", data);
           setToken(data.data.accessToken);
-          // window.location.href = DEFAULT_REDIRECT_ROUTE;
           return;
         }
         console.log("OAuth error:", event.data.error);
@@ -34,7 +29,7 @@ export default function useOAuth(
 
     window.addEventListener("message", handleMessage);
     return () => window.removeEventListener("message", handleMessage);
-  }, [setError]);
+  }, [setError, setToken]);
 
   return {
     loginWithGoogle,
